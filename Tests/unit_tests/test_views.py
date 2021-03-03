f454773c190148a38526160b9b42adbb99fc2a6d
Victor Nguyen <49082974+VictorNguyen031900@users.noreply.github.com>
2021-03-03 13:54:05 -0800

    Update test_views.py

--- Tests/unit_tests/test_views.py
+++ Tests/unit_tests/test_views.py
@@ -13,6 +13,10 @@ This is set in manage.py. We need to explicitly set it for tests to work with py
 """
 
 sys.path.append(os.path.join(os.getcwd(), 'Application'))
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "python_webapp_django.settings"
+)
 django.setup()
 
 class ViewTest(TestCase):
